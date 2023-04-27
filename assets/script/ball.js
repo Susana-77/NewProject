99cd4e5ef244321c3090c4885ef29022bfe9c7ec
Susana-77 <susanalsm@163.com>
2023-04-27 11:47:44 +0800

    把pause行为和开始游戏行为分成两个脚本

--- assets/script/ball.js
+++ assets/script/ball.js
@@ -49,55 +49,14 @@ cc.Class({
     this.initVel = 0;
     this.initPhysics();
     cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.boost, this);
-
     this.goBtn.node.on("click", this.start, this);
-    this.pauseBtn.node.on("click", this.pauseGame, this);
-    this.pauseBtn.node.x = 10000;
-
-    // 获取要控制的节点
-    const groupNode1 = this.blockGroup1;
-    const groupNode2 = this.blockGroup2;
-    const groupNode3 = this.blockGroup3;
-    const groupNode4 = this.blockGroup4;
-    const groupNode5 = this.blockGroup5;
-    const groupNode6 = this.blockGroup6;
-    const groupNode7 = this.blockGroup7;
-    const groupNode8 = this.blockGroup8;
-
-    // 获取节点上的 cc.AudioSource 组件
-    this.audioSource1 = groupNode1.getComponent("blockgroup").audioSource;
-    this.audioSource12 = groupNode1.getComponent("blockgroup").audioSource2;
-
-    this.audioSource2 = groupNode2.getComponent("blockgroup").audioSource;
-    this.audioSource22 = groupNode2.getComponent("blockgroup").audioSource2;
-
-    this.audioSource3 = groupNode3.getComponent("blockgroup").audioSource;
-    this.audioSource32 = groupNode3.getComponent("blockgroup").audioSource2;
-
-    this.audioSource4 = groupNode4.getComponent("blockgroup").audioSource;
-    this.audioSource42 = groupNode4.getComponent("blockgroup").audioSource2;
-
-    this.audioSource5 = groupNode5.getComponent("blockgroup").audioSource;
-    this.audioSource52 = groupNode5.getComponent("blockgroup").audioSource2;
-
-    this.audioSource6 = groupNode6.getComponent("blockgroup").audioSource;
-    this.audioSource62 = groupNode6.getComponent("blockgroup").audioSource2;
-
-    this.audioSource7 = groupNode7.getComponent("blockgroup").audioSource;
-    this.audioSource72 = groupNode7.getComponent("blockgroup").audioSource2;
-
-    this.audioSource8 = groupNode8.getComponent("blockgroup").audioSource;
-    this.audioSource82 = groupNode8.getComponent("blockgroup").audioSource2;
 
     cc.director.pause();
   },
 
   onDestroy() {
     cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.boost, this);
-    // this.node.on("mousedown", this.start, this);
     this.goBtn.node.on("click", this.start, this);
-    this.pauseBtn.node.on("click", this.pauseGame, this);
-
   },
 
   //初始化物理引擎
@@ -120,53 +79,7 @@ cc.Class({
   start(e) {
     cc.director.resume();
     this.goBtn.node.x = -10000;
-    this.pauseBtn.node.x = 404;
-  },
-
-  pauseGame() {
-    
-    if (cc.director.isPaused()) {
-      this.audioSource1.resume();
-      this.audioSource2.resume();
-      this.audioSource3.resume();
-      this.audioSource4.resume();
-      this.audioSource5.resume();
-      this.audioSource6.resume();
-      this.audioSource7.resume();
-      this.audioSource8.resume();
-
-      this.audioSource12.resume();
-      this.audioSource22.resume();
-      this.audioSource32.resume();
-      this.audioSource42.resume();
-      this.audioSource52.resume();
-      this.audioSource62.resume();
-      this.audioSource72.resume();
-      this.audioSource82.resume();
-
-      cc.director.resume();
-
-    } else {
-      this.audioSource1.pause();
-      this.audioSource2.pause();
-      this.audioSource3.pause();
-      this.audioSource4.pause();
-      this.audioSource5.pause();
-      this.audioSource6.pause();
-      this.audioSource7.pause();
-      this.audioSource8.pause();
-
-      this.audioSource12.pause();
-      this.audioSource22.pause();
-      this.audioSource32.pause();
-      this.audioSource42.pause();
-      this.audioSource52.pause();
-      this.audioSource62.pause();
-      this.audioSource72.pause();
-      this.audioSource82.pause();
-
-      cc.director.pause();
-    }
+    this.pauseBtn.node.x = 884;
   },
 
   //加速
--- assets/script/pause.js
+++ assets/script/pause.js
@@ -1,51 +1,60 @@
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
-        pauseBtn: cc.Button,
-        defaultSprite: cc.SpriteFrame,
-        pressedSprite: cc.SpriteFrame,
+  properties: {
+    pauseBtn: cc.Button,
+    defaultSprite: cc.SpriteFrame,
+    pressedSprite: cc.SpriteFrame,
 
-        blockGroup1: {
-            default: null,
-            type: cc.Node,
-          },
-          blockGroup2: {
-            default: null,
-            type: cc.Node,
-          },
-          blockGroup3: {
-            default: null,
-            type: cc.Node,
-          },
-          blockGroup4: {
-            default: null,
-            type: cc.Node,
-          },
-          blockGroup5: {
-            default: null,
-            type: cc.Node,
-          },
-          blockGroup6: {
-            default: null,
-            type: cc.Node,
-          },
-          blockGroup7: {
-            default: null,
-            type: cc.Node,
-          },
-          blockGroup8: {
-            default: null,
-            type: cc.Node,
-          }
+    pauseMenu: {
+      default: null,
+      type: cc.Node,
     },
 
-    onLoad () {
-        this.isButtonPressed = false;
-        this.pauseBtn.normalSprite = this.defaultSprite;
-        this.pauseBtn.node.on('click', this.onButtonClicked, this);
+    blockGroup1: {
+      default: null,
+      type: cc.Node,
+    },
+    blockGroup2: {
+      default: null,
+      type: cc.Node,
+    },
+    blockGroup3: {
+      default: null,
+      type: cc.Node,
+    },
+    blockGroup4: {
+      default: null,
+      type: cc.Node,
+    },
+    blockGroup5: {
+      default: null,
+      type: cc.Node,
+    },
+    blockGroup6: {
+      default: null,
+      type: cc.Node,
+    },
+    blockGroup7: {
+      default: null,
+      type: cc.Node,
+    },
+    blockGroup8: {
+      default: null,
+      type: cc.Node,
+    }
+  },
 
-        // 获取要控制的节点
+  onLoad() {
+    console.log(this.pauseMenu.x);
+    this.pauseBtn.node.x = 10000;
+    this.pauseMenu.x = 10000;
+
+    this.isButtonPressed = false;
+    this.pauseBtn.normalSprite = this.defaultSprite;
+    this.pauseBtn.node.on('click', this.onButtonClicked, this);
+
+    // 获取要控制的节点
     const groupNode1 = this.blockGroup1;
     const groupNode2 = this.blockGroup2;
     const groupNode3 = this.blockGroup3;
@@ -80,60 +89,56 @@ cc.Class({
     this.audioSource8 = groupNode8.getComponent("blockgroup").audioSource;
     this.audioSource82 = groupNode8.getComponent("blockgroup").audioSource2;
 
-    },
+  },
 
-    onButtonClicked () {
-      console.log("123")
-        this.isButtonPressed = !this.isButtonPressed;
-        if (this.isButtonPressed) {
-            this.pauseBtn.normalSprite = this.pressedSprite;
-        } else {
-            this.pauseBtn.normalSprite = this.defaultSprite;
-        }
-    },
+  onButtonClicked() {
+    this.isButtonPressed = !this.isButtonPressed;
+    if (this.isButtonPressed) {
+      this.pauseBtn.normalSprite = this.pressedSprite;
+      this.pauseMenu.x = 0;
 
-    pauseGame() {
-        if (cc.director.isPaused()) {
-          this.audioSource1.resume();
-          this.audioSource2.resume();
-          this.audioSource3.resume();
-          this.audioSource4.resume();
-          this.audioSource5.resume();
-          this.audioSource6.resume();
-          this.audioSource7.resume();
-          this.audioSource8.resume();
-    
-          this.audioSource12.resume();
-          this.audioSource22.resume();
-          this.audioSource32.resume();
-          this.audioSource42.resume();
-          this.audioSource52.resume();
-          this.audioSource62.resume();
-          this.audioSource72.resume();
-          this.audioSource82.resume();
-    
-          cc.director.resume();
-    
-        } else {
-          this.audioSource1.pause();
-          this.audioSource2.pause();
-          this.audioSource3.pause();
-          this.audioSource4.pause();
-          this.audioSource5.pause();
-          this.audioSource6.pause();
-          this.audioSource7.pause();
-          this.audioSource8.pause();
-    
-          this.audioSource12.pause();
-          this.audioSource22.pause();
-          this.audioSource32.pause();
-          this.audioSource42.pause();
-          this.audioSource52.pause();
-          this.audioSource62.pause();
-          this.audioSource72.pause();
-          this.audioSource82.pause();
-    
-          cc.director.pause();
-        }
-      },
+      this.audioSource1.pause();
+      this.audioSource2.pause();
+      this.audioSource3.pause();
+      this.audioSource4.pause();
+      this.audioSource5.pause();
+      this.audioSource6.pause();
+      this.audioSource7.pause();
+      this.audioSource8.pause();
+
+      this.audioSource12.pause();
+      this.audioSource22.pause();
+      this.audioSource32.pause();
+      this.audioSource42.pause();
+      this.audioSource52.pause();
+      this.audioSource62.pause();
+      this.audioSource72.pause();
+      this.audioSource82.pause();
+
+      cc.director.pause();
+    } else {
+      this.pauseBtn.normalSprite = this.defaultSprite;
+      this.pauseMenu.x = 10000;
+
+      this.audioSource1.resume();
+      this.audioSource2.resume();
+      this.audioSource3.resume();
+      this.audioSource4.resume();
+      this.audioSource5.resume();
+      this.audioSource6.resume();
+      this.audioSource7.resume();
+      this.audioSource8.resume();
+
+      this.audioSource12.resume();
+      this.audioSource22.resume();
+      this.audioSource32.resume();
+      this.audioSource42.resume();
+      this.audioSource52.resume();
+      this.audioSource62.resume();
+      this.audioSource72.resume();
+      this.audioSource82.resume();
+
+      cc.director.resume();
+    }
+  },
 });
